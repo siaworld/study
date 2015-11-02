18bc795bdc62169d82a864022988d10ba7cf8247
siaworld <hjbaek0314@gmail.com>
2015-11-03 01:44:20 +0900

    not yet resolved

--- SWTraining/src/Solution.java
+++ SWTraining/src/Solution.java
@@ -6,7 +6,36 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+	static long stringSimilarity(String a) {
+    	
+    	int n = a.length();
+    	long result = (long)n;
+        List<Integer> z = new ArrayList<Integer>();
+        
+        int i,l,r;i=0;l=0;r=0;
+        z.add(0);
+        //z.add(0);
+    	for (i = 1, l = 0, r = 0; i < n; ++i) {
+            z.add(0);
+            if (i <= r)
+                z.add(i, Math.min(r - i + 1, z.get(i - l)));
+            while (i + z.get(i) < n && a.charAt(z.get(i)) == a.charAt(z.get(i)+i))
+                z.set(i, z.get(i)+1);
+            if (i + z.get(i) - 1 > r)
+            {
+            	l = i;
+            	r = i + z.get(i) - 1;
+            }
+                
+        }
+    	for(int cnt : z)
+    	{
+    		result += cnt;
+    	}
+        return result;
+    }
+	
+	public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     
     	Scanner sc = new Scanner(System.in);
@@ -15,24 +44,14 @@ public class Solution {
     	T = sc.nextInt();
     	for(int i = 0 ; i <T; ++i)
     	{
-    		char[] inputA = sc.nextLine().toCharArray();
-    		char[] inputB = sc.nextLine().toCharArray();
-    		int k,j;k=0;j=0;
-            boolean find=false;
-    		for( k=0 ; k < inputA.length ; k++ )
-    		{
-    			for( j=0; j < inputB.length; j++)
-    			{
-    				if(inputA[k] == inputB[j])
-    				{
-    					k = inputA.length;
-                        j = inputB.length;
-                        find = true;
-    				    break;
-    				}
-    			}
-    		}
-    		if(find)
+    		String inputA = sc.next();
+    		String inputB = sc.next();
+    		String combAB = inputA.concat("A").concat(inputB);
+    		
+    		long result = stringSimilarity(combAB);
+    		
+    		
+    		if(result > inputA.length()+inputB.length())
                 System.out.println("YES");
             else
     			System.out.println("NO");
